a6dcfdf3ef7afcba26c5eac8c6444f0891a727e4
Samarth Mishra <samarth1mishra@gmail.com>
2024-01-31 21:43:52 +0530

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router=require('express').Router();
 const User=require('../models/user.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 //Sign UP
 
 router.post('/register',async(req,res)=>{
