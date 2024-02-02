922cafab9c67f4ec76f076ccef8fccb385e9cbd4
Samarth Mishra <samarth1mishra@gmail.com>
2024-02-03 01:52:46 +0530

    changed

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ const path = require('path');
 app.use(express.json());
 app.use(cors());
 
-// Serve static files from the React app
-app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
 
 // Define your API routes
 app.use('/api/v1', auth);
 app.use('/api/v2', list);
 
 // Define the catch-all route to serve the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
 
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 Connection();
 
 const PORT = process.env.PORT || 1000;
