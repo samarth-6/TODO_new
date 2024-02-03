ec137c695fb77a8107dbd189950f0ff7890dc6cd
Samarth Mishra <samarth1mishra@gmail.com>
2024-02-03 23:18:50 +0530

    Merge remote changes

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const path = require('path');
 app.use(express.json());
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+
+// Define your API routes
 app.use('/api/v1', auth);
 app.use('/api/v2', list);
 
-app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// Define the catch-all route to serve the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
+
 Connection();
 
-
-app.listen(1000, () => {
-  console.log("Server is running on port 1000");
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
--- frontend/src/components/signup/signup.js
+++ frontend/src/components/signup/signup.js
@@ -14,7 +14,7 @@ const Signup = () => {
   const Submit=async(e)=>{
     e.preventDefault();
     await axios.post(`${window.location.origin}/api/v1/register`,Inputs).then((response)=>{
-      if (response.data.message === "User Already exists") {
+      if (response.data.message === "User Already Exists") {
         alert(response.data.message);
       } else {
         alert(response.data.message);
